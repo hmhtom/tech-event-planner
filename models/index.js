8d1f0e24e15565d865a06df95d407097b61a4557
Safa Zgolli <fifi.zgolli89@gmail.com>
2022-10-27 13:17:14 -0400

    most of the models created

---
+++ models/index.js
@@ -0,0 +1,43 @@
+const User = require('./User');
+const Event = require('./Event');
+const Comment = require('./Comment');
+const Category = require('./Category');
+const Participant = require('./Participant');
+
+
+
+
+Category.hasMany(Event,{
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE'
+
+});
+Event.belongsTo(Category, {
+    foreignKey: 'event_id',
+    onDelete: 'CASCADE'
+  });
+
+Event.belongsToMany(User, {
+    // Define the third table needed to store the foreign keys
+    through: {
+      model: Participant,
+      unique: true,
+      
+    },
+    foreignKey: 'event_id',
+    otherKey: 'user_id'
+
+
+});
+User.belongsToMany(Event, {
+    // Define the third table needed to store the foreign keys
+    through: {
+      model: Participant,
+      unique: true,
+    },
+    foreignKey: 'user_id',
+    otherKey: 'event_id'
+
+
+});
+module.exports = {User, Event, Participant, Category};
